fb939c54160ff196a1ab89f9df3247e548df2904
tobbel42 <tobiasdegrossmann@gmx.de>
2022-11-09 09:37:43 +0100

    cleanup

--- pydocker/src/mongoCnct.py
+++ pydocker/src/mongoCnct.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import os
+import csv
 
 #username and pw are passed via the .env file for extra sec B)
 username = os.environ.get('USER') 
@@ -9,23 +10,19 @@ password = os.environ.get('PASSWORD')
 client = MongoClient("mongodb+srv://" + username + ":" + password + "@anuradb.rrmor6f.mongodb.net/?retryWrites=true&w=majority")
 
 #selecting the DataBase 
-db = client.test
+db = client.anuracsv
 
-#selecting the Connection
+#selecting the Collection
 coll = db.dummyData
 
-#just some random dummy data
-dummyData = [
-	{"name": "Halley's Comet", "officialName": "1P/Halley", "orbitalPeriod": 75, "radius": 3.4175, "mass": 2.2e14},
-	{"name": "Wild2", "officialName": "81P/Wild", "orbitalPeriod": 6.41, "radius": 1.5534, "mass": 2.3e13},
-	{"name": "Comet Hyakutake", "officialName": "C/1996 B2", "orbitalPeriod": 17000, "radius": 0.77671, "mass": 8.8e12},
-]
 
-#inserting the data into the collection 
-result = coll.insert_many(dummyData)
+dummyData = []
 
-#printing the ids of the inserted objects
-print(result.inserted_ids)
+with open('vitaldata.csv') as csvfile:
+	reader = csv.DictReader(csvfile)
+	for row in reader:
+		coll.insert_one(row)
+
 
 #close connection, cause we care 
 client.close()
